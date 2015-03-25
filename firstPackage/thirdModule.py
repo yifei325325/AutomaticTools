0d40926add50edb9d6321604794b597cadd2f02a
yifei325325 <yifei1193@163.com>
2015-03-25 22:59:28 +0800

    新建web 小爬虫程序

--- firstPackage/thirdModule.py
+++ firstPackage/thirdModule.py
@@ -3,14 +3,20 @@
 '''
 Created on 2015年3月24日
 
-@author: Administrator
-'''
-s = u'中文'
-s1=s.encode('utf-8')
-print s
-def fun(a):
-    return a
-b = fun('hello')
+@author: kenny
 
-print type(b)
-print b
+little web spider
+'''
+import urllib
+import webbrowser as web
+
+url = 'http://www.163.com'
+
+content = urllib.urlopen(url).read()
+
+#print content
+
+f1 = open('e:/163.com.html','w')#打开本地e:\163.com.html，没有则创建一个该文件
+f1.write(content)
+web.open_new_tab('e:/163.com.html')
+print 'complete'
